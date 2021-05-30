2278f5386503e3cb78cf8a1477e1e9da41cc7650
Julie Foucher <jumiloarking@gmail.com>
2021-05-30 18:34:37 +0200

    Insertion du choix de question depuis la base

--- inscription.php
+++ inscription.php
@@ -6,6 +6,16 @@ include 'entete.php';
 ?>
 
 <body>
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;port=3307;dbname=gbaf;charset=utf8', 'root', 'root');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+?>
 
 <!--formulaire de création de compte -->
 <form action="inscription.php" method="post">
@@ -21,13 +31,15 @@ include 'entete.php';
 <input type="password" name="passwordconfirm" required>
 <label for="question">Question secrète utilisée pour réinitialiser votre mot de passe</label>
 <select name="question">
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
+<?php
+$choice_question=$bdd->query('SELECT question FROM questions');
+while ($choice = $choice_question->fetch())
+{
+    ?>
+    <option value="<?php echo $choice ['question'];?>"><?php echo $choice ['question'];?></option>
+<?php
+}
+?>
 </select>
 <label for="reponse">Votre réponse:</label>
 <input type="text" name="reponse" required>
@@ -45,18 +57,5 @@ $username= $_POST["username"];
 $password= $_POST["firstnamepasswordconfirm"];
 $question= $_POST["question"];
 $reponse= $_POST["reponse"];
-
-try
-{
-	$bdd = new PDO('mysql:host=localhost;port=3307;dbname=gbaf;charset=utf8', 'root', 'root');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
-
-$enregistrement =$bdd->prepare('INSERT INTO users(nom, prenom, username,password,question,reponse) VALUES [?,?,?,?,?,?]);
-
-header("Location:index.php");
 ?>
 </body>
