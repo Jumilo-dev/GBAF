2bc1a59b6917b1fac127211a6516afaf97aea3af
Julie Foucher <jumiloarking@gmail.com>
2021-06-02 00:55:37 +0200

    Recuperation des donnes du formulaire en php

--- inscription.php
+++ inscription.php
@@ -1,12 +1,26 @@
 
 <?php
-$title ="Inscription";
-include 'entete.php';
+//*Vérifier que le formulaire est envoyé, et que les champs sont completés
+if(!empty($_POST)){
 
-?>
-
-<body>
-<?php
+	if(isset($_POST["name"],$_POST["firstname"],$_POST["username"],$_POST["password"],$_POST["question"],$_POST["reponse"])
+		&& !empty($_POST["name"]) && !empty ($_POST["firstname"])&& !empty ($_POST["username"])&& !empty ($_POST["password"])&& !empty ($_POST["question"])&& !empty ($_POST["reponse"])
+	){ //*Recupérer et protéger les données, faire les contrôles
+        
+        $name= strip_tags($_POST["name"]);
+        $firstname= strip_tags($_POST["firstname"]);
+        $username= strip_tags($_POST["username"]);
+        //*hasher le mot de passe (revoir l'algo utilisé)
+		$password= password_hash($_POST["password"],PASSWORD_DEFAULT);
+        //*trouver solution pour envoyer l'id_question au lieu de la question
+        $question= strip_tags($_POST["question"]);
+        $reponse= strip_tags($_POST["reponse"]);
+    
+    }
+    
+}    	
+	//*Connexion à la base	
+    
 try
 {
 	$bdd = new PDO('mysql:host=localhost;port=3307;dbname=gbaf;charset=utf8', 'root', 'root');
@@ -15,24 +29,59 @@ catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
+//*Requete
+$sql= "INSERT INTO users(``,`nom`,`prenom`,`username`,`password`,`question`,`reponse`) VALUES ('',:nom, :prenom, :username, '$password',:question,:reponse)";
+//*Requete preparée
+$query = $bdd->prepare($sql);
+
+$query->bindValue(":nom", $name, PDO::PARAM_STR);
+$query->bindValue(":prenom", $firstname, PDO::PARAM_STR);
+$query->bindValue(":username", $username, PDO::PARAM_STR);
+$query->bindValue(":question", $question, PDO::PARAM_STR);
+$query->bindValue(":reponse", $reponse, PDO::PARAM_STR);
+
+$query->execute();
+//*message d'erreur si le formulaire est incomplet (à compléter plus tard pour indiquer quel champ est incomplet)
+/*}else{
+    die("Le formulaire est incomplet")
+*/
+
+$title ="Inscription";
+include 'entete.php';
+
 ?>
 
+
+
+
+<body>
+<h1>Inscription</h1>
+
 <!--formulaire de création de compte -->
-<form action="inscription.php" method="post">
+<form  method="post">
+<div>
 <label for="name">Nom </label>
 <input type="text" name="name" required>
+</div>
+<div>
 <label for="firstname">Prénom </label>
 <input type="text" name="firstname" required>
+</div>
+<div>
 <label for="username">Nom d'utilisateur</label>
 <input type="text" name="username" required>
+</div>
+<div>
 <label for="password">Choisissez un mot de passe</label>
 <input type="password" name="password" required>
-<label for="passwordconfirm">Confirmer le mot de passe</label>
-<input type="password" name="passwordconfirm" required>
+</div>
+<div>
 <label for="question">Question secrète utilisée pour réinitialiser votre mot de passe</label>
 <select name="question">
 <?php
 $choice_question=$bdd->query('SELECT question FROM questions');
+
+
 while ($choice = $choice_question->fetch())
 {
     ?>
@@ -41,21 +90,15 @@ while ($choice = $choice_question->fetch())
 }
 ?>
 </select>
+</div>
+<div>
 <label for="reponse">Votre réponse:</label>
 <input type="text" name="reponse" required>
-
+</div>
+<div>
 <button type="submit">Valider</button>
+</div>
 
 
 </form>
-<?php
-
-
-$nom= $_POST["name"];
-$prenom= $_POST["firstname"];
-$username= $_POST["username"];
-$password= $_POST["firstnamepasswordconfirm"];
-$question= $_POST["question"];
-$reponse= $_POST["reponse"];
-?>
 </body>
